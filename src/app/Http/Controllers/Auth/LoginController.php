84d4ceddca8e5cdda1ec565992a42d31a8913281
nishida namiki <ymdnmk1201@gmail.com>
2026-06-29 17:49:46 +0900

    refactor: ログイン処理から未使用のremember指定を削除

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -13,7 +13,7 @@ class LoginController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials, $request->boolean('remember'))) {
+        if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
             return redirect()->intended('/');
